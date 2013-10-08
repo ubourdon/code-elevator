67ba88508a90b578e0d4e7bc62f41475137d084e
ubourdon <bourdon.ugo@gmail.com>
2013-10-08 23:27:38 +0200

    add: open/close, up/down building method

--- app/model/Building.scala
+++ app/model/Building.scala
@@ -1,6 +1,6 @@
 package model
 
-case class Building(score: Int = 0, peopleWaitingTheElevator: Vector[Int] = Vector(0, 0, 0, 0, 0, 0), floor: Int = 0, peopleInTheElevator: Int = 0, doorIsOpen: Boolean = false) {
+case class Building(score: Int = 0, peopleWaitingTheElevator: Vector[Int] = Vector(0, 0, 0, 0, 0, 0), floor: Int = 0, peopleInTheElevator: Int = 0, doorIsOpen: Boolean = false, maxFloor: Int = 5) {
     /*private val maxFloor = 5
 
     private var floor: Int = 0
@@ -9,8 +9,23 @@ case class Building(score: Int = 0, peopleWaitingTheElevator: Vector[Int] = Vect
     */
 
     def addUser(): Unit = ()
-    def up(): Building = throw new IllegalStateException("not implemented")                                  /*if(floor < maxFloor) floor = floor + 1$*/
-    def down(): Building = throw new IllegalStateException("not implemented")                                 //if(floor > 0) floor = floor - 1
-    def open(): Building = throw new IllegalStateException("not implemented")
-    def close(): Building = throw new IllegalStateException("not implemented")
+
+    // TODO use validation ???
+    def up(): Building =
+        if(doorIsOpen) throw new IllegalStateException("the door is opened")
+        else if(floor < maxFloor) this.copy(floor = this.floor + 1)
+        else throw new IllegalStateException("the floor is reached maximum")
+
+    def down(): Building =
+        if(doorIsOpen) throw new IllegalStateException("the door is opened")
+        else if(floor > 0) this.copy(floor = this.floor - 1)
+        else throw new IllegalStateException("the floor is reached 0")
+
+    def open(): Building =
+        if(doorIsOpen) throw new IllegalStateException("doors are already opened")
+        else this.copy(doorIsOpen = true)
+
+    def close(): Building =
+        if(!doorIsOpen) throw new IllegalStateException("doors are already closed")
+        else this.copy(doorIsOpen = false)
 }
--- test/model/BuildingTest.scala
+++ test/model/BuildingTest.scala
@@ -5,6 +5,55 @@ import org.scalatest.matchers.ShouldMatchers
 
 class BuildingTest extends FunSuite with ShouldMatchers {
 
-//    test("")
+    test("building.up should increment floor by 1") {
+        Building().up() should be (Building(floor = 1))
+    }
+
+    test("if the floor is reached maximum, building.up throw IllegalStateException") {
+        val exception = evaluating { Building(maxFloor = 0).up() } should produce[IllegalStateException]
+        exception should have ('message("the floor is reached maximum"))
+    }
+
+    test("if door is open, building.up throwIllegalSateException") {
+        val exception = evaluating { Building(doorIsOpen = true).up() } should produce[IllegalStateException]
+        exception should have ('message("the door is opened"))
+    }
+
+    test("building.down should decrement floor by 1") {
+        Building(floor = 1).down() should be (Building(floor = 0))
+    }
+
+    test("if the floor is reached 0, building.down throw IllegalStateException") {
+        val exception = evaluating { Building().down() } should produce[IllegalStateException]
+        exception should have ('message("the floor is reached 0"))
+    }
+
+    test("if door is open, building.down throw IllegalSateException") {
+        val exception = evaluating { Building(doorIsOpen = true).down() } should produce[IllegalStateException]
+        exception should have ('message("the door is opened"))
+    }
+
+    test("if door is opened, building.close should close the door") {
+        Building(doorIsOpen = true).close() should be (Building())
+    }
+
+    test("if door is closed, building.close should throw IllegalStateException") {
+        val exception = evaluating { Building().close() } should produce[IllegalStateException]
+        exception should have ('message("doors are already closed"))
+    }
+
+    test("if door is closed, building.open should open the door") {
+        Building().open() should be (Building(doorIsOpen = true))
+    }
+
+    test("if door is opened, building.open should throw IllegalStateException") {
+        val exception = evaluating { Building(doorIsOpen = true).open() } should produce[IllegalStateException]
+        exception should have ('message("doors are already opened"))
+    }
+
+    // test addUser
+        //ajoute un user dans le building
+            // si limite atteinte - on ajoute pas
+            // sinon add user avec floor de départ et floor souhaité
 
 }
